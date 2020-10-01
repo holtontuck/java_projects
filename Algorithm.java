adb5b62fa1bb13faec92f573be7332e02054bd6b
holtontuck <32709810+holtontuck@users.noreply.github.com>
2020-10-01 14:03:37 -0400

    Delete Algorithm.java

--- Algorithm.java
+++
@@ -1,6 +0,0 @@
-package encryptdecrypt;
-
-abstract class Algorithm {
-    abstract void decrypt(String message, int key, boolean toFile, String _fileName);
-        abstract void encrypt(String message, int key, boolean toFile, String _fileName);
-}
